a487bd0c302dce0e0fd4cb9abbf43642f36900b0
Alejandro Sorza <Jasorza5@misena.edu.co>
2019-06-07 11:18:37 -0500

    1

---
+++ Data/WebApplication1Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+    public class WebApplication1Context : DbContext
+    {
+        public WebApplication1Context (DbContextOptions<WebApplication1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Categoria> Categoria { get; set; }
+    }
